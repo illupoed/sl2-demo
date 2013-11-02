a10311890a14159235335fc7d473afedd356924c
Tom Landvoigt <tomlandvoigt@gmx.de>
2013-11-02 21:14:31 +0100

    Adding a very early example app

---
+++ example app/hash1.js
@@ -0,0 +1,20 @@
+/***********************************/
+// generated from: hello
+/***********************************/
+define
+	( function(require, exports, module) 
+		{
+    		var $$std$prelude = require("std/prelude.sl");
+			var Log = require("std/debuglog.sl"); 
+			; 
+			var $main = function () 
+				{
+					var $5 = Log.$print;
+					var $4 = sl.hash1.var2;
+					var $main = $5($4);
+					return $main 
+				}(); 
+			; 
+			exports.$main = $main
+		}
+	);
---
+++ example app/sl_init.js
@@ -0,0 +1,6 @@
+var require = 
+	{ baseUrl: "file:/home/norcain/workspace/sl2-demo/example app/"
+	, paths: {std : "lib/" } // has to be configurable
+	};
+
+var sl = {};

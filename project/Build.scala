9a933a07302c094530f673f4d75d8cb8a9da8d77
Tom Landvoigt <tomlandvoigt@gmx.de>
2014-02-28 20:50:28 +0100

    added scala macro paradise to subproject sl
    because sbt can not use the Build.scala of the subproject

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,8 @@ object ApplicationBuild extends Build {
 
   val appName         = "webui_sl"
   val appVersion      = "1.0-SNAPSHOT"
-
+  val paradiseVersion = "2.0.0-M3"
+  
   val json4sNative = "org.json4s" %% "json4s-native" % "3.2.4"
 
   val appDependencies = Seq(
@@ -16,11 +17,31 @@ object ApplicationBuild extends Build {
     anorm
   )
 
-  lazy val sl2 = Project(id = "sl2", base = file("sl2"))
+  lazy val sl2 = Project(
+    id = "sl2",
+    base = file("sl2"), 
+    settings = Project.defaultSettings ++ Seq(
+      resolvers += Resolver.sonatypeRepo("snapshots"),
+	  resolvers += Resolver.sonatypeRepo("releases"),
+      addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full)
+    ) ++ Seq(
+      libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
+      libraryDependencies ++= (
+        if (scalaVersion.value.startsWith("2.10")) List("org.scalamacros" % "quasiquotes" % paradiseVersion cross CrossVersion.full)
+        else Nil
+      )
+    )
+  )
 
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    unmanagedClasspath in Compile <+= (baseDirectory) map { bd => bd / "sl-files" }
- ).dependsOn(sl2)
+  val main = play.Project(
+    appName,
+    appVersion,
+    appDependencies
+  ).settings(
+    unmanagedClasspath in Compile <+= (baseDirectory) map { bd => bd / "sl-files" },
+	resolvers += Resolver.sonatypeRepo("snapshots"),
+	resolvers += Resolver.sonatypeRepo("releases"),
+	addCompilerPlugin("org.scalamacros" % "paradise" % paradiseVersion cross CrossVersion.full)
+  ).dependsOn(sl2)
 
 }

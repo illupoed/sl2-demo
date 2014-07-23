1ac0e3fd2efb0fa5fee768ebf6c814c9a57433da
Tom Landvoigt <tomlandvoigt@gmx.de>
2014-07-23 02:32:34 +0200

    new version of sl2 and some new apps
     - due to changes in sl2 Application.scala now extends a new trait which
       holds the helper to call annotated functions
     - renamed old koch.scala.html to test.scala.html <- it was my testing
       playground
     - koch.scala.html generates again koch snowflakes <- it is now possible
       to choose how many of them
     - added tictactoe app <- you can play tic tac toe here <- it is a test
       for anotated functions <- the used scala function is defined in
    Examples.scala
     - of course we need the apropriate routes

--- app/callable_functions/Examples.scala
+++ app/callable_functions/Examples.scala
@@ -2,6 +2,7 @@ package callable_functions
 
 import de.tuberlin.uebb.sl2.slmacro.sl_function
 import java.util.Calendar
+import java.util.Random
 import java.text.SimpleDateFormat
 
 object Examples {
@@ -25,12 +26,137 @@ object Examples {
       k + l + 3
     }
 
-  @sl_function def blub( k: Int = 3 ): Int = 2 + k 
-  
+  @sl_function def blub( k: Int = 3 ): Int = 2 + k
+
   @sl_function def getserverinfo(): String = {
     val today = Calendar.getInstance().getTime()
-    val simpleFormat = new SimpleDateFormat("hh:mm:ss")
-    "Server Time: " + simpleFormat.format(today)
+    val simpleFormat = new SimpleDateFormat( "hh:mm:ss" )
+    "Server Time: " + simpleFormat.format( today ) + " Scala " + util.Properties.versionString
+  }
+
+  /**
+   * crappy ai for tic tac toe
+   */
+  @sl_function def tictactoesolver( input: Seq[Int] ): Either[String, Pair[String, Int]] = {
+    val unset = 0
+    val user_set = 1
+    val comp_set = 2
+    var to_set = -1
+
+    if ( input.length != 9 || input.filter( ( x: Int ) => !( x <= 2 && x >= 0 ) ).length > 0 )
+      return Left( """<span style="color:#f00">Error: Corrupted Input</span>""" )
+
+    if ( cheated( input ) )
+      return Left( """<span style="color:#f00">You have cheated ... Are you serious?</span>""" )
+
+    if ( checkRows( input, user_set ) || checkCols( input, user_set ) || checkDiag( input, user_set ) )
+      return Left( """<span style="color:#090">You WON!!!</span>""" )
+
+    if ( input.filter( ( x: Int ) => x == 0 ).length == 0 )
+      return Left( """<span style="color:#fbb117">DRAW</span>""" )
+
+    // disallow 2 in a row
+    if ( unset == input( 0 ) &&
+      ( ( input( 1 ) == user_set && input( 2 ) == user_set )
+        || ( input( 3 ) == user_set && input( 6 ) == user_set )
+        || ( input( 4 ) == user_set && input( 8 ) == user_set ) ) )
+      to_set = 0
+
+    if ( unset == input( 2 ) &&
+      ( ( input( 1 ) == user_set && input( 0 ) == user_set )
+        || ( input( 5 ) == user_set && input( 8 ) == user_set )
+        || ( input( 4 ) == user_set && input( 6 ) == user_set ) ) )
+      to_set = 2
+
+    if ( unset == input( 6 ) &&
+      ( ( input( 0 ) == user_set && input( 3 ) == user_set )
+        || ( input( 7 ) == user_set && input( 8 ) == user_set )
+        || ( input( 4 ) == user_set && input( 2 ) == user_set ) ) )
+      to_set = 6
+
+    if ( unset == input( 8 ) &&
+      ( ( input( 6 ) == user_set && input( 7 ) == user_set )
+        || ( input( 5 ) == user_set && input( 2 ) == user_set )
+        || ( input( 4 ) == user_set && input( 0 ) == user_set ) ) )
+      to_set = 8
+
+    if ( unset == input( 4 ) &&
+      ( ( input( 1 ) == user_set && input( 7 ) == user_set )
+        || ( input( 5 ) == user_set && input( 3 ) == user_set )
+        || ( input( 8 ) == user_set && input( 0 ) == user_set )
+        || ( input( 6 ) == user_set && input( 2 ) == user_set )
+      ) )
+      to_set = 4
+
+    if ( unset == input( 1 ) &&
+      ( ( input( 0 ) == user_set && input( 2 ) == user_set )
+        || ( input( 4 ) == user_set && input( 7 ) == user_set ) ) )
+      to_set = 1
+
+    if ( unset == input( 3 ) &&
+      ( ( input( 0 ) == user_set && input( 6 ) == user_set )
+        || ( input( 5 ) == user_set && input( 4 ) == user_set ) ) )
+      to_set = 3
+
+    if ( unset == input( 7 ) &&
+      ( ( input( 1 ) == user_set && input( 4 ) == user_set )
+        || ( input( 6 ) == user_set && input( 8 ) == user_set ) ) )
+      to_set = 7
+
+    if ( unset == input( 5 ) &&
+      ( ( input( 2 ) == user_set && input( 8 ) == user_set )
+        || ( input( 3 ) == user_set && input( 4 ) == user_set ) ) )
+      to_set = 5
+
+    // nothing urgend try to get the middle
+    if ( unset == input( 4 ) )
+      to_set = 4
+
+    // choose one by rand
+    if ( to_set == -1 ) {
+      val to_choose = for ( i <- 0 to 8 if input( i ) == unset ) yield i
+
+      val rand = new Random( System.currentTimeMillis() )
+      to_set = to_choose( rand.nextInt( to_choose.length ) )
+    }
+
+    val new_board = for ( i <- 0 to 8 ) yield { if ( i == to_set ) comp_set else input( i ) }
+
+    if ( checkRows( new_board, comp_set ) || checkCols( new_board, comp_set ) || checkDiag( new_board, comp_set ) )
+      return Right( Pair( """<span style="color:#090">HAHA I have won1!!1</span>""", to_set ) )
+    else
+      return Right( Pair( "", to_set ) )
+
+  }
+
+  def checkRows( input: Seq[Int], n: Int ): Boolean = {
+    ( input( 0 ) == n && input( 1 ) == n && input( 2 ) == n ) ||
+      ( input( 3 ) == n && input( 4 ) == n && input( 5 ) == n ) ||
+      ( input( 6 ) == n && input( 7 ) == n && input( 8 ) == n )
+  }
+
+  def checkCols( input: Seq[Int], n: Int ): Boolean = {
+    ( input( 0 ) == n && input( 3 ) == n && input( 6 ) == n ) ||
+      ( input( 1 ) == n && input( 4 ) == n && input( 7 ) == n ) ||
+      ( input( 2 ) == n && input( 5 ) == n && input( 8 ) == n )
+  }
+
+  def checkDiag( input: Seq[Int], n: Int ): Boolean = {
+    ( input( 0 ) == n && input( 4 ) == n && input( 8 ) == n ) ||
+      ( input( 6 ) == n && input( 4 ) == n && input( 2 ) == n )
+  }
+
+  def cheated( input: Seq[Int] ): Boolean = {
+    var own = 0;
+    var player = 0;
+
+    for ( i <- input ) {
+      if ( i == 1 ) player = player + 1
+
+      if ( i == 2 ) own = own + 1
+    }
+
+    player != own + 1
   }
 
 }
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,16 +8,16 @@ import org.json4s.JsonDSL.{ list2jvalue => _, _ }
 import org.json4s.native.JsonMethods.{ render => jrender, parse => jparse, _ }
 import play.api.data._
 import play.api.data.Forms._
-import de.tuberlin.uebb.sl2.slmacro.sl_function
+import de.tuberlin.uebb.sl2.slmacro.InvokeHelper
 
-object Application extends Controller {
+object Application extends Controller with InvokeHelper {
 
   def index = Action {
     Ok( views.html.index( "Your new application is ready." ) )
   }
 
-  def koch = Action {
-    Ok( views.html.koch( true ) )
+  def koch(n: Int = 3) = Action {
+    Ok( views.html.koch( n ) )
   }
 
   def foo = Action {
@@ -39,13 +39,21 @@ object Application extends Controller {
   def checkFunCall = Action {
     Ok( views.html.checkFunCall() )
   }
-
+  
+  def tictactoe = Action {
+    Ok( views.html.tictactoe() )
+  }
+  
+  def test = Action {
+    Ok( views.html.test( true ) )
+  }
+  
   def slCalls = Action { request =>
 
     val obj_name = request.body.asFormUrlEncoded.get( "object_name" ).head
     val fun_name = request.body.asFormUrlEncoded.get( "function_name" ).head
     val params = request.body.asFormUrlEncoded.get( "params" ).head
-
+    
     try {
       val fun_result = invokeObjectMethodByName( obj_name, fun_name, jparse( params ).children: _* )
       Ok( compact( jrender( JObject( JField( "result", fun_result ) ) ) ) ).as( "application/json" )
@@ -55,36 +63,4 @@ object Application extends Controller {
     }
 
   }
-
-  /**
-   * Invokes a method of an object by its name
-   *
-   * this should help the controller to call the right function
-   *
-   * this method can only call methods of type String* -> String
-   * (because this is the only use case i have)
-   *
-   * @see https://stackoverflow.com/questions/1913092/getting-object-instance-by-string-name-in-scala
-   * @see https://stackoverflow.com/questions/2060395/is-there-any-scala-feature-that-allows-you-to-call-a-method-whose-name-is-stored
-   *
-   * @example invokeObjectMethodByName("main.bar.Woot", "fac", "10")
-   */
-  def invokeObjectMethodByName( object_name_with_path: String, function_name: String, params: JValue* ): JValue =
-    {
-      val c = Class.forName( object_name_with_path + "$" )
-      val clazz = c.getField( "MODULE$" ).get( c )
-      val param_classes = params.map( ( _ ) => classOf[JValue] /*_.getClass()*/ )
-
-      clazz.getClass.getMethod( function_name, param_classes: _* ).invoke( clazz, params: _* ).asInstanceOf[JValue]
-    }
-
-  //  def expr = Action {
-  //    Ok( views.html.expr() )
-  //  }
-  //
-  //  def makeObj( clazz: Int, vals: JValue* ) =
-  //    {
-  //      val fields: List[( String, JValue )] = ( "_cid" -> JInt( clazz ) ) :: ( vals.zipWithIndex.map( { case ( v, idx ) => ( ( "_var" + idx ) -> v ) } ).toList )
-  //      JObject( fields )
-  //    }
 }
